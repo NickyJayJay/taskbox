8dc10a8f96d8485786d84c5281b967114486910d
Nicholas Jacob <nicholas.jacob@guitarcenter.com>
2021-08-20 16:32:56 -0700

    Initial commit

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -11,11 +11,14 @@ export default function useCachedResources() {
     async function loadResourcesAndDataAsync() {
       try {
         SplashScreen.preventAutoHideAsync();
-
+    
         // Load fonts
         await Font.loadAsync({
-          ...FontAwesome.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          percolate: require('../assets/icon/percolate.ttf'),
+          'NunitoSans-Bold': require('../assets/font/NunitoSans-Bold.ttf'),
+          'NunitoSans-Italic': require('../assets/font/NunitoSans-Italic.ttf'),
+          NunitoSans: require('../assets/font/NunitoSans-Regular.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
